85ad05a8dc9d0895d52ce20ee9f568d374e5d7af
Jan Romann <jan.romann@uni-bremen.de>
2021-05-20 20:13:42 +0200

    tests: add test for /.well-known/core discovery

--- test/wot-discovery_spec.js
+++ test/wot-discovery_spec.js
@@ -1,6 +1,8 @@
-
-var WoTDiscoveryNode = require("../nodes/wot-discovery.js");
-var helper = require("node-red-node-test-helper");
+const WoTDiscoveryNode = require("../nodes/wot-discovery.js");
+const injectNode = require("@node-red/nodes/core/common/20-inject.js");
+const helper = require("node-red-node-test-helper");
+const coap = require("coap");
+const url = require("uri-js");
 
 describe("WoTScriptingNode", function () {
     beforeEach(function (done) {
@@ -14,17 +16,85 @@ describe("WoTScriptingNode", function () {
     });
 
     it("should be loaded", function (done) {
-        var flow = [
+        const flow = [
             {
                 id: "n1",
                 type: "wot-discovery",
             },
         ];
         //need to register nodes in order to use them
-        var testNodes = [WoTDiscoveryNode];
+        const testNodes = [WoTDiscoveryNode];
         helper.load(testNodes, flow, function () {
             done();
         });
     });
 
+    describe("CoAP Discovery", function () {
+
+        it("should support multicast discovery from /.well-known/core", function (done) {
+            const flow = [
+                {
+                    id: "n1",
+                    type: "inject",
+                    wires: [["n2"]],
+                    once: true,
+                    payload: "",
+                    payloadType: "none",
+                },
+                {
+                    id: "n2",
+                    type: "wot-discovery",
+                    useCoap: true,
+                    coreURI: true,
+                    tdURI: false,
+                    coapUseIPv6: true,
+                    coapIPv6Address: "all",
+                    msgOrContext: "msg",
+                    wires: [["n3"]],
+                },
+                {
+                    id: "n3",
+                    type: "end-test-node",
+                    name: "end-test-node",
+                },
+            ];
+
+            function endTestNode(RED) {
+                function EndTestNode(n) {
+                    RED.nodes.createNode(this, n);
+                    this.on("input", function (msg) {
+                        msg.thingDescription
+                            .should.eql({blah: "hi"}); // Not a valid TD yet...
+                        done();
+                    });
+                }
+                RED.nodes.registerType(
+                    "end-test-node",
+                    EndTestNode
+                );
+            }
+
+            const settings = {type: "udp6", reuseAddr: true, multicastAddress: "ff02::1", multicastInterface: "::%eth2"};
+            const server = new coap.createServer(settings);
+
+            server.on('request', function(req, res) {
+                const path = url.parse(req.url).path;
+
+                if (path === "/.well-known/core") {
+                    res.setOption("Content-Format", "application/link-format");
+                    res.end('</test>;rt="wot.thing";ct=432');
+                } else if (path === "/test") {
+                    res.setOption("Content-Format", "application/json");
+                    const td = {blah: "hi"};
+                    res.end(JSON.stringify(td));
+                }
+              })
+
+            const testNodes = [injectNode, WoTDiscoveryNode, endTestNode];
+            helper.load(testNodes, flow, function () {
+                server.listen(5683);
+            });
+        });
+
+    });
 });
