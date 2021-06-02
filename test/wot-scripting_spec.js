6d68eae7c9f6321d36c15747bc69ae7f015c2d16
Jan Romann <jan.romann@uni-bremen.de>
2021-06-03 00:12:19 +0200

    tests: add tests for wot-scripting node

--- test/wot-scripting_spec.js
+++ test/wot-scripting_spec.js
@@ -1,40 +1,120 @@
 const WoTScriptingNode = require("../nodes/wot-scripting.js");
 const helper = require("node-red-node-test-helper");
+const coap = require("coap");
+const url = require("uri-js");
+const testTD = require('./test.td.json');
+
+let coapServer;
 
 describe("WoTScriptingNode", function () {
     beforeEach(function (done) {
+        coapServer = new coap.createServer();
+        coapServer.listen(5683);
         helper.startServer(done);
     });
 
     afterEach(function (done) {
         helper.unload().then(function () {
+            coapServer.close();
             helper.stopServer(done);
         });
     });
 
-    it("should be loaded", function (done) {
+    it(`should be able to read properties`, function (done) {
         const flow = [
             {
                 id: "n1",
                 type: "wot-scripting",
-                "affordanceName": "temperature",
-                "affordanceType": "readProperty",
+                operationType: "readProperty",
+                affordanceName: "status",
+                filterMode: "affordanceName",
+                msgOrContext: "msg",
+                wires: [["n2"]],
+            },
+            {
+                id: "n2",
+                type: "end-test-node",
+                name: "end-test-node",
             },
         ];
-        //need to register nodes in order to use them
-        const testNodes = [WoTScriptingNode];
+
+        function endTestNode(RED) {
+            function EndTestNode(n) {
+                RED.nodes.createNode(this, n);
+                this.on("input", function (msg) {
+                    msg.payload
+                        .should.eql("on");
+                    done();
+                });
+            }
+            RED.nodes.registerType(
+                "end-test-node",
+                EndTestNode
+            );
+        }
+
+        coapServer.on('request', function (req, res) {
+            const path = url.parse(req.url).path;
+
+            req.method.should.eql("GET");
+            path.should.eql("/status");
+            res.setOption("Content-Format", "application/json");
+            res.end(JSON.stringify("on"));
+        });
+
+        const testNodes = [WoTScriptingNode, endTestNode];
         helper.load(testNodes, flow, function () {
-            done();
+            const n1 = helper.getNode("n1");
+            n1.emit("input", { thingDescription: testTD, });
         });
     });
 
-    it("should do something", function (done) {
-        const flow = [];
+    it(`should be able to invoke actions`, function (done) {
+        const flow = [
+            {
+                id: "n1",
+                type: "wot-scripting",
+                operationType: "invokeAction",
+                filterMode: "@type",
+                affordanceType: "test:toggle",
+                msgOrContext: "msg",
+                wires: [["n2"]],
+            },
+            {
+                id: "n2",
+                type: "end-test-node",
+                name: "end-test-node",
+            },
+        ];
 
-        //need to register nodes in order to use them
-        const testNodes = [];
+        function endTestNode(RED) {
+            function EndTestNode(n) {
+                RED.nodes.createNode(this, n);
+                this.on("input", function (msg) {
+                    msg.payload
+                        .should.eql("on");
+                    done();
+                });
+            }
+            RED.nodes.registerType(
+                "end-test-node",
+                EndTestNode
+            );
+        }
+
+        coapServer.on('request', function (req, res) {
+            const path = url.parse(req.url).path;
+            
+            req.method.should.eql("POST");
+            path.should.eql("/toggle");
+            res.setOption("Content-Format", "application/json");
+            res.end(JSON.stringify("on"));
+        });
+
+        const testNodes = [WoTScriptingNode, endTestNode];
         helper.load(testNodes, flow, function () {
-            done();
+            const n1 = helper.getNode("n1");
+            n1.emit("input", { thingDescription: testTD, });
         });
     });
 
