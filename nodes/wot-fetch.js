b0b2c0697b37fda1fbabafb25c2d398ae530e9e9
Jan Romann <jan.romann@uni-bremen.de>
2021-09-02 23:59:37 +0200

    feat(wot-fetch): Implement caching for TD URLs

--- nodes/wot-fetch.js
+++ nodes/wot-fetch.js
@@ -15,6 +15,7 @@ module.exports = function (RED) {
     } = require("@node-wot/binding-coap");
 
     const statusClearingDelay = 5000;
+    const tdCacheTime = 15 * 60 * 1000; // TODO: Should be configurable
 
     const servient = new Servient();
     servient.addClientFactory(new HttpClientFactory(null));
@@ -27,6 +28,8 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         const node = this;
         let clearingTimeout;
+        const tdCache = {};
+
         node.on("input", function (msg) {
             const tdUrl = msg.tdUrl || config.tdUrl;
             const outputVar =
@@ -34,45 +37,71 @@ module.exports = function (RED) {
             const outputVarType =
                 msg.outputVarType || config.outputVarType || "msg";
 
-            node.status({
-                fill: "blue",
-                shape: "dot",
-                text: `Fetching TD from ${tdUrl}`,
-            });
-
-            WoTHelpers.fetch(tdUrl)
-                .then(async (td) => {
-                    switch (outputVarType) {
-                        case "msg":
-                            msg[outputVar] = td;
-                            node.send(msg);
-                            break;
-                        case "flow":
-                        case "global":
-                            node.context()[outputVarType].set(outputVar, td);
-                            break;
-                        default:
-                            throw new Error(
-                                "Wrong output variable type defined."
-                            );
-                    }
-                    _setStatusTimeout();
-                    node.status({
-                        fill: "green",
-                        shape: "ring",
-                        text: "Success!",
-                    });
-                })
-                .catch((err) => {
-                    node.error("Fetch error:", err);
-                    _setStatusTimeout();
-                    node.status({
-                        fill: "red",
-                        shape: "ring",
-                        text: `Error fetching TD from ${tdUrl}`,
-                    });
+            if (tdCache[tdUrl] != undefined) {
+                _setStatusTimeout();
+                node.status({
+                    fill: "green",
+                    shape: "ring",
+                    text: `Used cached TD for ${tdUrl}`,
                 });
+                _handleTd(tdCache[tdUrl]);
+            } else {
+                _fetchTd();
+            }
+
+            function _fetchTd() {
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: `Fetching TD from ${tdUrl}`,
+                });
+
+                WoTHelpers.fetch(tdUrl)
+                    .then(async (td) => {
+                        _handleTd(td);
+                        _cacheTd(tdUrl, td);
+                        _setStatusTimeout();
+                        node.status({
+                            fill: "green",
+                            shape: "ring",
+                            text: "Success!",
+                        });
+                    })
+                    .catch((err) => {
+                        node.error("Fetch error:", err);
+                        _setStatusTimeout();
+                        node.status({
+                            fill: "red",
+                            shape: "ring",
+                            text: `Error fetching TD from ${tdUrl}`,
+                        });
+                    });
+            }
+
+            function _handleTd(td) {
+                switch (outputVarType) {
+                    case "msg":
+                        msg[outputVar] = td;
+                        node.send(msg);
+                        break;
+                    case "flow":
+                    case "global":
+                        node.context()[outputVarType].set(outputVar, td);
+                        break;
+                    default:
+                        throw new Error(
+                            "Wrong output variable type defined."
+                        );
+                }
+            }
         });
+
+        function _cacheTd(tdUrl, td) {
+            tdCache[tdUrl] = td;
+            setTimeout(() => {
+                delete tdCache[tdUrl];
+            }, tdCacheTime);
+        }
 
         function _setStatusTimeout() {
             if (clearingTimeout) {
